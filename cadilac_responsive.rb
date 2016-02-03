86f21667e155f3354c48ce2997d28e9140e282d8
pktqa <pktqa@usnycpww9456.global.publicisgroupe.net>
2016-02-03 12:57:12 -0500

    Cadilac Responsive

---
+++ cadilac_responsive.rb
@@ -0,0 +1,82 @@
+require 'watir-webdriver'
+require 'webdriver-user-agent'
+require 'csv'
+require 'watir-scroll'
+require 'time'
+
+
+
+
+ 
+
+def setupBrowserStack
+
+include Selenium
+
+caps = WebDriver::Remote::Capabilities.new
+caps[:os] = "Windows"
+caps[:name] = "Watir WebDriver"
+caps[:browser] = "firefox"
+caps[:browser_version] = "20"
+caps["browserstack.debug"] = "true"
+
+$b= Watir::Browser.new(:remote,
+  :url => "http://zaqwsx1:Fs54nwmULt7BaSTosZxi@hub.browserstack.com/wd/hub",
+  :desired_capabilities => caps)
+
+
+$b.goto "http://www.daregreatly.com/explore/culture/visual-art/latoya-ruby-frazier"
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+puts setupBrowserStack
+
+
+csv_text = File.read('responsive.csv',encoding: "iso-8859-1:UTF-8")
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|  
+sleep 4
+loc1= "#{row['sizeA']}"
+loc2= "#{row['sizeB']}"
+loc3= "#{row['device']}"
+
+sleep 5
+puts loc1
+
+$b.window.resize_to(loc1, loc2)
+
+sleep 2
+$b.screenshot.save loc3<<".png"
+
+
+sleep 2
+
+
+
+end
+
+
+
+
+
+
+
+ 
+
+
+$b.quit
+

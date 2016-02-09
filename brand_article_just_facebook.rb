a6e60cb1e81ac0ee7a73f8ca4d306da574186447
pktqa <pktqa@usnycpww9456.global.publicisgroupe.net>
2016-02-09 10:35:05 -0500

    pushed

--- brand_article_just_facebook.rb
+++ brand_article_just_facebook.rb
@@ -14,11 +14,10 @@ def setupBrowserStack
 include Selenium
 
 caps = WebDriver::Remote::Capabilities.new
-caps['browser'] = 'Firefox'
-caps['browser_version'] = '43.0'
-caps['os'] = 'Windows'
-caps['os_version'] = '7'
-caps['resolution'] = '1024x768'
+caps[:os] = "Windows"
+caps[:name] = "Watir WebDriver"
+caps[:browser] = "firefox"
+caps[:browser_version] = "20"
 caps["browserstack.debug"] = "true"
 
 $b= Watir::Browser.new(:remote,

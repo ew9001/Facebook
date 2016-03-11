3e38a991c7e1472dcd94fa5c7f825c7fbfd2ceab
pktqa <pktqa@usnycpww9456.global.publicisgroupe.net>
2016-03-11 17:41:11 -0500

    go to URL

--- Brand/mobile.rb
+++ Brand/mobile.rb
@@ -14,7 +14,7 @@ browser = Watir::Browser.new(:remote,
   :url => "http://zaqwsx1:Fs54nwmULt7BaSTosZxi@hub.browserstack.com/wd/hub",
   :desired_capabilities => caps)
 
-browser.goto "https://author.crestca.pgsitecore.com/en-ca/products/toothpaste?#viewtype:gridview/facets:collection=crest-sensi/category:toothpaste"
+browser.goto "http://author.crestca.pgsitecore.com/en-ca/products/toothpaste?#viewtype:gridview/facets:collection=crest-sensi/category:toothpaste"
 
 puts browser.title
 browser.screenshot.save browser.title<<".png"
